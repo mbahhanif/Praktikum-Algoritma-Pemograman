87dc4c38a48808e2e950323159cc3b7e4f354e5d
Mbah Bejo <hanifraharjo07@gmail.com>
2025-10-28 23:59:19 +0700

    Add initial content to Tugas_Praktikum_9.py

---
+++ Tugas_Praktikum_9.py
@@ -0,0 +1,10 @@
+# Nama = Muhammad Hanif Raharjo
+# NPM = 2505060074
+# Rombel = 4
+
+# Latihan
+
+# 1. Buah buahan
+buah = ['apel', 'mangga', 'jeruk', 'anggur', 'pisang']
+
+# a. Tambahkan 'semangka' di akhir list
