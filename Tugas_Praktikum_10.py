446d6dc7f7e2f942e27907041d4aad8987e9c104
Mbah Bejo <hanifraharjo07@gmail.com>
2025-11-05 12:47:46 +0700

    Update task link in Tugas_Praktikum_10.py

--- Tugas_Praktikum_10.py
+++ Tugas_Praktikum_10.py
@@ -1,7 +1,7 @@
 # Nama : Muhammad Hanif Raharjo
 # NPM : 2505060074
 # Rombel : 4
-# Link Tugas :
+# Link Tugas : https://colab.research.google.com/drive/19vVKMe-rOMSpfb-2iuyJ2gze7oIrlX-y
 
 # Latihan
 
